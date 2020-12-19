404196ae39258c1a6d4acb340e7fd8f746874f07
Ronin <bistrin.matvey@gmail.com>
2020-12-19 13:54:19 +0300

    Add constructor in BlowfishEncrypter

--- include/blowfish.hpp
+++ include/blowfish.hpp
@@ -12,8 +12,9 @@ class BlowfishEncrypter
 {
 public:
     BlowfishEncrypter();
-    void encrypt_block(uint32_t &left, uint32_t &right);
-    void decrypt_block(uint32_t &left, uint32_t &right);
+    BlowfishEncrypter(uint32_t *key, int len); //Creates object and expand given key
+    void encrypt_block(uint32_t &left, uint32_t &right); //Encrypting two blocks with own key
+    void decrypt_block(uint32_t &left, uint32_t &right); //Decrypting two blocks with own key
     void key_expand(uint32_t *key, int len);
     void encrypt_file(std::string in_filepath,std::string out_filepath = "");
     void decrypt_file(std::string in_filepath,std::string out_filepath = "");
